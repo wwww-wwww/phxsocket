55f0b0ec1e388bf066629ec75bfe1c56b828a857
w <wvvwvvvvwvvw@gmail.com>
2020-10-30 07:40:33 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+with open("requirements.txt") as fh:
+  install_requires = fh.read()
+
+setup(
+  name="phxsocket",
+  version="0.0.1",
+  author="wwwwwwww",
+  author_email="wvvwvvvvwvvw@gmail.com",
+  description="Websocket client for Phoenix Elixir",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/wwww-wwww/phxsocket",
+  install_requires=install_requires,
+  packages=find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+  ],
+  python_requires=">=3.6",
+)
